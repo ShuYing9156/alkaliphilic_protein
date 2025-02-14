1db142d8f82225a15f9d236f8a3ba00ba3fb2cc8
shuying <ruby2015095231128@gmail.com>
2025-02-14 11:07:51 +0800

    The results of the wab page and API are different.

--- Bio_Entrez.py
+++ Bio_Entrez.py
@@ -1,12 +1,35 @@
 from Bio import Entrez
+from Bio import SeqIO
 
 Entrez.email = 'ruby2015095231128@gmail.com'
 
-handle = Entrez.efetch( db = 'protein', term = 'alkaliphilic', rettype='gb', retmode='text' )
+handle = Entrez.esearch( db = 'protein', term = 'alkaliphilic' )
 record = Entrez.read(handle)
 
-print(record)
+idlist = list(record['IdList'])
+
+#print(record)
 #print(record.keys())
-#print(record['Count'])
+#print(record['IdList'])
+#print(type(idlist))
+print(idlist)
 #print(record['DbInfo'].keys())
 #print(record['DbInfo']['MenuName'])
+
+handle.close()
+
+id_str = ",".join(idlist)
+
+handle2 = Entrez.efetch( db = 'protein', id = id_str, rettype="fasta", retmode="text" )
+#record2 = Entrez.read(handle2)
+
+record2 = list(SeqIO.parse(handle2, "fasta"))
+handle2.close()
+
+for records in record2:
+    print(f"ID: {records.id}")
+    print(f"描述: {records.description}")
+    print(f"序列: {records.seq}")
+    print(f"序列長度: {len(records.seq)}\n")
+
+
