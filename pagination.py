109d513f3bf296d635f3f66bec6173296587d674
shuying <ruby2015095231128@gmail.com>
2025-02-14 11:36:53 +0800

    Paginate the data of alkaliphilic protein from NCBI

---
+++ pagination.py
@@ -0,0 +1,27 @@
+from Bio import Entrez
+
+# 設置電子郵件
+Entrez.email = "ruby2015095231128@gmail.com"
+
+# 設置查詢參數
+db = "protein"
+term = "alkaliphilic"
+retmax = 100  # 每次查詢返回 100 條記錄
+
+# 獲取總記錄數
+handle = Entrez.esearch(db=db, term=term)
+record = Entrez.read(handle)
+total_records = int(record["Count"])
+handle.close()
+#print(total_records)
+#print(record)
+
+
+# 分頁查詢
+for start in range(0, total_records, retmax):
+    handle = Entrez.esearch(db=db, term=term, retmax=retmax, retstart=start)
+    record = Entrez.read(handle)
+    handle.close()
+    
+    # 打印當前頁的 ID
+    #print(f"Records {start + 1} to {start + len(record['IdList'])}: {record['IdList']}")
