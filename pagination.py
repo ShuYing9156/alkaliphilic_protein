ed72cdc25b6a781483cccf1dd6f6e8fd7ed6b97a
shuying <ruby2015095231128@gmail.com>
2025-02-25 13:25:16 +0800

    Get the data from NCBI.

--- pagination.py
+++ pagination.py
@@ -1,33 +1,33 @@
 from Bio import Entrez
 
-# 設置電子郵件
+## 設置電子郵件
 Entrez.email = "ruby2015095231128@gmail.com"
 
-# 設置查詢參數
+## 設置查詢參數
 db = "protein"
 term = "alkaliphilic"
 retmax = 100  # 每次查詢返回 100 條記錄
 
-# 獲取總記錄數
+## 獲取總記錄數
 handle = Entrez.esearch(db=db, term=term)
 record = Entrez.read(handle)
-total_records = int(record["Count"])
-handle.close()
-#print(total_records)
 #print(record)
+total_records = int(record["Count"])
+#print(total_records)
+handle.close()
 
 
-# 分頁查詢
+
+## 分頁查詢
 for start in range(0, total_records, retmax):
     handle = Entrez.esearch(db=db, term=term, retmax=retmax, retstart=start)
     record = Entrez.read(handle)
     handle.close()
 
-    #將id寫入檔案
+    # 將id寫入檔案
     with open('idlist.txt', mode='a+', encoding='utf-8') as idlist_file:
         for id in record['IdList']:
             idlist_file.write(f'{id}\n')
     
     # 印出出當前頁的id
     print(f"Records {start + 1} to {start + len(record['IdList'])}")
-    ''': {record['IdList']}'''
